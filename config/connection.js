9c873b1a1f1dd73b73b51c1a5b26e596e0eb52d5
jeffmlb91 <jeffmlb91@gmail.com>
2022-01-27 01:16:08 -0500

    first commit

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+//Requiring sequelize
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+// Set connection to DB
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DV_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    prot: 3005,
+  }
+);
+module.exports = sequelize;
