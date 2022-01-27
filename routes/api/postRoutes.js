31c362a0fa8a6a9587cd8b15f047c5345da6a63c
jeffmlb91 <jeffmlb91@gmail.com>
2022-01-27 12:19:51 -0500

    updated server.js

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -1,105 +1,143 @@
-const sequelize = require('../../config/connection');
-const { Module } = require('express').Router();
-const { Post, User, Comment, Vote } = require('../../models');
-const router = require('./commentRoutes');
+const sequelize = require("../../config/connection");
+const { Module } = require("express").Router();
+const { Post, User, Comment, Vote } = require("../../models");
+const router = require("./commentRoutes");
 
 //Setting up all the routes
 //PERFORMING THE CRUD OPERATION
 //FIRST GET ALL the POSTS available
-router.get('/', (req, res) => {
-    Post.findAll({
-        attribute: [
-            'id',
-            'title',
-            'field',
-            'created_at',
-        ],
+router.get("/", (req, res) => {
+  Post.findAll({
+    attribute: ["id", "title", "field", "created_at"],
 
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'user_id', 'comment_text', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ],
-        order: [['created_at', 'DESC']]
-    })
-    .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "user_id", "comment_text", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+    order: [["created_at", "DESC"]],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // SECOND WHEN GET a POST BY IT'S ID
-router.get('/', (req, res) => {
-    Post.findOne({
-        where: { 
-            id: req.params.id
-        },
-        attribute: [
-            'id',
-            'field',
-            'title',
-            'created_at',
-        ],
+router.get("/", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attribute: ["id", "field", "title", "created_at"],
 
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'user_id', 'comment_text', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            },
-        ]
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "user_id", "comment_text", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "THIS ID HAS NO POST" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//CREATE A POST CRUD OPERATION WITH
+router.Post("/", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//APPROVING A VOTE
+router.put("/approve", (req, res) => {
+  Post.approve(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+//CRUD UPDATE POST
+
+router.put("/:id", (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+  .then((dbPostData) => {
+    if (!dbPostData) {
+      res.status(404).json({ message: "THIS ID HAS NO POST" });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+
+//CRUD OPERATION DELETE POST DATA
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
     })
     .then(dbPostData => {
-        if (!dbPostData) {
-            res.status(404).json({ message: 'THIS ID HAS NO POST'});
+        if (dbPostData) {
+            res.status(404).json({ message: "THIS ID HAS NO POST" });
             return;
         }
         res.json(dbPostData);
     })
     .catch(err => {
-        console.log(err);res.status(500).json(err);
-    });
-});
-
-//CREATE A POST CRUD OPERATION WITH
-router.Post('/', (req, res) => {
-    Post.create({
-        title: req.body.title,
-        post_url: req.body.post_url,
-        user_id: req.body.user_id
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
         console.log(err);
-        res.status(500).json(err);
+        res.status(500),json(err);
     });
 });
 
-//APPROVING A VOTE
-router.put('/approve', (req, res) => {
-    Post.approve(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
-});
-
-
+module.exports = router;
